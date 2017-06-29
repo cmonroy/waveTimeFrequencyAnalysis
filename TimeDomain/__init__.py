0f805d40c10ed22d975df2eadea733a78b5bd83e
Guillaume de Hauteclocque <guillaume.de-hauteclocque@bureauveritas.com>
2017-06-29 10:50:01 +0200

    read removed from TimeSignals (duplicates with df_Reader in Reader)

--- TimeDomain/__init__.py
+++ TimeDomain/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
-from .TimeSignals import getPSD, read , bandPass, slidingFFT,  fftDf, comparePlot
+from .TimeSignals import getPSD, bandPass, slidingFFT,  fftDf, comparePlot
 
 from .upCross import upCrossMinMax, plotUpCross
 
