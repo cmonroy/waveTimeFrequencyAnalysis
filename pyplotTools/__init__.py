ebce9a84bb450618e08f93f828d86c261d0caf6e
Guillaume de Hauteclocque <guillaume.de-hauteclocque@bureauveritas.com>
2018-12-20 11:19:27 +0100

    [FUN] scatterPlot : possibility to color by 2D histogram (kde is quite slow with n > 1e4)

--- pyplotTools/__init__.py
+++ pyplotTools/__init__.py
@@ -3,4 +3,4 @@ from .dfPlot import dfSlider, dfAnimate, dfSurface, dfIsoContour
 from .pyplotTools import newColorIterator, markerCycle, newMarkerIterator, linestyleCycle
 from .pyplotTools import newLinestyleIterator, pyplotLegend, autoscale_y
 from .surfacePlot import mapFunction
-from .scatterPlot import kde_scatter
+from .scatterPlot import kde_scatter, density_scatter
