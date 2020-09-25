efea7b9e00d32d599071b7bd4599cd1cdfd96543
abenhamou <alexis.benhamou@bureauveritas.com>
2020-09-25 15:29:08 +0200

    [ENH] Interpolate: add fill_value option for values outside bounds

--- interpolate/linInterp.py
+++ interpolate/linInterp.py
@@ -9,9 +9,21 @@ from scipy.interpolate import InterpolatedUnivariateSpline
 
 """
 
-def InterpolateSpline(xx, yy, kind='linear', n=0, ext='extrapolate'):
+def InterpolateSpline(xx, yy, kind='linear', n=0, ext='extrapolate',fill_value=None):
     """ Get interpolation spline with linear or log interpolation
     """
+    
+    #Use fill_value outside bounds
+    if (ext=='const') and (fill_value is not None):
+        if not hasattr(fill_value,'__len__'): fill_value = [fill_value,fill_value]
+        if len(fill_value)!=2: raise ValueError('"fill_value" should have 2 values')
+        eps = 1e-12
+        xx = np.array(xx)
+        np.insert(xx,0,xx[0]-eps)
+        np.append(xx,[xx[-1]+eps])
+        yy=np.array(yy)
+        np.insert(yy,0,fill_value[0])
+        np.append(yy,[fill_value[1]])
     
     if kind=='linear':
         spline = InterpolatedUnivariateSpline( xx , yy, k=1, ext=ext)
@@ -21,6 +33,7 @@ def InterpolateSpline(xx, yy, kind='linear', n=0, ext='extrapolate'):
         ispos = (xx>0.0) & (yy>0.0)
         logx = np.log10(xx[ispos])
         logy = np.log10(yy[ispos])
+
         lin_spline = InterpolatedUnivariateSpline(logx, logy, k=1, ext=ext)
         
         if n==0:
